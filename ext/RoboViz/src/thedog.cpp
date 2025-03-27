e1dd3581c3c1ff875729469b190006c64ab3c721
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2025-03-27 13:57:55 -0400

    added clamped options as comment

--- ext/RoboViz/src/thedog.cpp
+++ ext/RoboViz/src/thedog.cpp
@@ -141,6 +141,11 @@ glm::vec3 Legs::ik_BR(const glm::vec3& r){
     float a2 = glm::acos(glm::clamp( (L1_2 + rlen2 - L2_2)/(2*L1*rlen)  , -1.f, 1.f ));
     float a3 = glm::acos(glm::clamp( (L1_2 + L2_2 - rlen2)/(2*L1*L2)    , -1.f, 1.f ));
     float a4 = glm::acos(glm::clamp( r.y/rlen                           , -1.f, 1.f ));
+    // return glm::vec3(
+    //     glm::clamp(a1       , glm::radians(-45.f), glm::radians(45.f)),
+    //     glm::clamp(a2+a4    , glm::radians(90.f-45.f), glm::radians(90.f+45.f)),
+    //     glm::clamp(a3       , glm::radians(90.f-45.f), glm::radians(90.f+45.f))
+    // );
     return glm::vec3(
         a1,
         a2+a4,
