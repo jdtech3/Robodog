a929b595732fd5b3b9ced1ac5b61e768b7d681ea
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2025-03-27 13:06:19 -0400

    clamped trig calculations so that legs do not disappear when invalid input is given

--- ext/RoboViz/src/thedog.cpp
+++ ext/RoboViz/src/thedog.cpp
@@ -138,9 +138,9 @@ glm::vec3 Legs::ik_BR(const glm::vec3& r){
     float rlen = glm::length(r);
     float rlen2 = rlen*rlen;
     float a1 = glm::atan(r.x, -r.z);
-    float a2 = glm::acos( (L1_2 + rlen2 - L2_2)/(2*L1*rlen) );
-    float a3 = glm::acos( (L1_2 + L2_2 - rlen2)/(2*L1*L2) );
-    float a4 = glm::acos( r.y/rlen );
+    float a2 = glm::acos(glm::clamp( (L1_2 + rlen2 - L2_2)/(2*L1*rlen)  , -1.f, 1.f ));
+    float a3 = glm::acos(glm::clamp( (L1_2 + L2_2 - rlen2)/(2*L1*L2)    , -1.f, 1.f ));
+    float a4 = glm::acos(glm::clamp( r.y/rlen                           , -1.f, 1.f ));
     return glm::vec3(
         a1,
         a2+a4,
